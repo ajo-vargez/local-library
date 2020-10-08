2fd3289b61b1668a0d252556825a00f5ac25466a
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-08 19:09:32 +0530

    Add a book and book instance model

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,8 @@
 from os import name
 from django.db import models
+from django.urls import reverse
+
+import uuid
 
 class Genre(models.Model):
     """Model representing a book's genre"""
@@ -8,5 +11,68 @@ class Genre(models.Model):
 
     def __str__(self):
         """String for representing the model object."""
-        
+
         return self.name
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of the book)."""
+
+    title = models.CharField(max_length=200)
+
+    # Foreign key used because a book can only have one author, but authors can have multiple books.
+    # Author as a string rather than an object because it hasn't been declared yet in the file.
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField(
+            "ISBN",
+            max_length=13,
+            help_text="13 Character <a href='https://www.isbn-international.org/content what-isbn'>ISBN number</a>"
+        )
+    
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined, so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book.")
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+
+        return reverse("book-detail", args=[str(self.id)])
+    
+
+class BookInstance(models.Model):
+    """Model representing a copy of the book (i.e, that can be borrowed from the library.)"""
+
+    id = models.UUIDField(
+            primary_key=True,
+            default=uuid.uuid4,
+            help_text="Unique ID for this particular book across whole library"
+        )
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintainance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'reserved'),
+    )
+
+    status = models.CharField(
+            max_length=1,
+            choices=LOAN_STATUS,
+            blank=True,
+            default='m',
+            help_text="Book availablity"
+        )
+    
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self) -> str:
+        return f"{self.id} ({self.book.title})"
