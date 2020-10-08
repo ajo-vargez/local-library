79b4853435dfe3046f3967e2af79d641112820bd
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-08 17:43:18 +0530

    Change admin site to library-admin/

--- local_library/urls.py
+++ local_library/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('library-admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent='True')),
 ]
