fc87cea9bba25c064a25056bdf62197e0eccd254
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-08 18:44:07 +0530

    Add a genre model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
+from os import name
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    """Model representing a book's genre"""
+
+    name = models.CharField(max_length=200, help_text="Enter a book genre (eg: Science Fiction)")
+
+    def __str__(self):
+        """String for representing the model object."""
+        
+        return self.name
