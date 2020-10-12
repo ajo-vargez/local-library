be75c0db60da2054cadc77de19d3e827aa21841a
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-12 16:53:20 +0530

    Organise the admin page by adding inline instances

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,14 +6,41 @@ from .models import Book, BookInstance, Author, Genre, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
 @register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 
 @register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "display_genre")
 
+    inlines = [BookInstanceInline]
+
+
 @register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ("book", "status", "due_back")
+    list_display = ("book", "status", "due_back", "id")
+    list_filter = ("status", "due_back")
+    fieldsets = (
+        (None, {
+            "fields": ("book", "imprint", "id")
+        }),
+        ("Availability", {
+            "fields": ("status", "due_back")
+        })
+    )
