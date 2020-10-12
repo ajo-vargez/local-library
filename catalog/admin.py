d0912728df2533a2c079b04d3399f3aa8f66657a
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-12 16:32:29 +0530

    Configure and register list views in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 from .models import Book, BookInstance, Author, Genre, Language
 
 
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
+
+@register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "date_of_birth", "date_of_death")
+
+@register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "display_genre")
+
+@register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back")
