dda0bc88fa35bdd0b1994960d2404cf66c9651d3
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-21 12:56:03 +0530

    Import css needed for buefy

--- library/src/main.js
+++ library/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import Buefy from 'buefy';
 
+// Import icons and styles
+import '@mdi/font/css/materialdesignicons.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'buefy/dist/buefy.css';
+
 Vue.use(Buefy);
 
 Vue.config.productionTip = false;
