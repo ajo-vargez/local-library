1e8ca43efa070b262748e4469c6b6b8273d3d86d
Ajo <mal.01ajo@yahoo.com>
2020-10-31 23:55:32 +0530

    Update the config file

--- library/vue.config.js
+++ library/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http:localhost',
+        target: 'http:localhost:8000',
         changeOrigin: true
       }
     }
